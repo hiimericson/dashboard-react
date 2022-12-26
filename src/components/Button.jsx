68217ca6ba8cee05a12d0f570f5e2b11edb02eed
hiimericson <105856244+hiimericson@users.noreply.github.com>
2022-12-27 02:44:02 +0800

    Source code

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Button = ({bgColor, color, size , text, borderRadius}) => {
+  return (
+    <button 
+    type='button'
+    style={{backgroundColor : bgColor, color, borderRadius
+    }} className={`text-${size} p-3
+    hover:drop-show-x1
+    `}>{text}</button>
+  )
+}
+
+export default Button
